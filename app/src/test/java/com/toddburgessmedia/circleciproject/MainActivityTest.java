084808ac0d7a06ed7a5e473aaaa58d0b41324511
Todd Burgess <todd@toddburgessmedia.com>
2016-04-29 16:01:08 -0400

    Broken test case

---
+++ app/src/test/java/com/toddburgessmedia/circleciproject/MainActivityTest.java
@@ -0,0 +1,29 @@
+package com.toddburgessmedia.circleciproject;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+/**
+ * Created by tburgess on 29/04/16.
+ */
+public class MainActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testFun () {
+        int x,y;
+        x = 0;y=4;
+        assert (x>y);
+    }
+}
